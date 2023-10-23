3053851d3092f01c47641ea00129b7d2ad6b2593
Sean Dorris <seanc.dorris@gmail.com>
2023-10-23 18:59:53 -0500

    initial repo setup

---
+++ socialJusticeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using social_justice_BE.Models;
+using System.Runtime.CompilerServices;
+
+public class socialJusticeDbContext : DbContext
+{
+
+
+
+    public socialJusticeDbContext(DbContextOptions<socialJusticeDbContext> context) : base(context)
+    {
+
+    }
+}
